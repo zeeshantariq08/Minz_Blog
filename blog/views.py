c180ad7a7cb05678ade126678298064fa6174cf9
zee <zeeshiq58@gmail.com>
2020-02-27 18:22:46 +0500

    add bootstrap templete

--- blog/views.py
+++ blog/views.py
@@ -2,12 +2,34 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
-def home(request):
-	return render(request,'blog/home.html')
+posts= [
+
+	{
+		'author':"Zeeshan Tariq",
+		"title":"Blog Post 1",
+		"content":"First Post Content",
+		"date":"August 20,2020",
+
+	}
+
+]
+
+
 
 
 def about(request):
-	return HttpResponse('<h1>About Blog</h1>')
+	return render(request,'blog/about.html')
+
+
+
+
+def home(request):
+	context = {
+		'posts':posts
+	}
+	return render(request,'blog/home.html',context)
+
+
+
